f131e2b01d55b15e37e0f02d16c80f7e250a43b9
Rustam Ormonov <php2497@gmail.com>
2025-05-01 11:42:13 +0500

    some changes

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,7 +22,7 @@ class TestController extends Controller
         // Savollarni tasodifiy aralashtirib, 30 ta tanlash
         $randomIds = collect($questionIds)
             ->shuffle() // Tasodifiy ravishda aralashtirish
-            ->take(72) // 30 ta savolni tanlash
+            ->take(180) // 30 ta savolni tanlash
             ->values();
 
         // Tanlangan savollarni olish
